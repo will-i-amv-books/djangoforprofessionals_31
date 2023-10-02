7bd147f88fbf6100ca03632ebdbb0457ec0b5822
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-02 13:43:38 -0300

    Load env vars with 'environs' package

--- code/ch3_books/config/settings.py
+++ code/ch3_books/config/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
