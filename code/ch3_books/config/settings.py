eb070956a9803287ecb01294236b66c76fd39531
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-02 14:01:58 -0300

    Add new env var 'DJANGO_DEBUG' to docker-compose

--- code/ch3_books/config/settings.py
+++ code/ch3_books/config/settings.py
@@ -30,10 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = env("DJANGO_DEBUG")
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
