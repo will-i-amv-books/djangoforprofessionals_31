f3d27077ce0dde38d7b2a17e2f88d02c12e99f43
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-03 10:30:42 -0300

    Add a template-based form and use its value to filter the queryset

--- code/ch3_books/books/views.py
+++ code/ch3_books/books/views.py
@@ -29,9 +29,10 @@ class SearchResultsListView(ListView):
     template_name = 'books/search_results.html'
 
     def get_queryset(self):
+        query = self.request.GET.get('q')
         return (
             Book.objects
             .filter(
-                Q(title__icontains='beginners') | Q(title__icontains='api')
+                Q(title__icontains=query) | Q(title__icontains=query)
             )
         )
