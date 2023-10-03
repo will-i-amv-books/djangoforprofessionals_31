b4eb0542ccc90dd8a65278c597f0cd3f6edb74ca
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-03 09:20:00 -0300

    Add configs related to media files

--- code/ch3_books/config/settings.py
+++ code/ch3_books/config/settings.py
@@ -141,6 +141,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 #########################################
 # User-defined
 #########################################
