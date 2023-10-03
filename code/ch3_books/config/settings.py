c448980e6777de71b801454d53522d16f861e750
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-03 12:12:57 -0300

    Force DEBUG type as boolean, otherwise unexpected things can happen

--- code/ch3_books/config/settings.py
+++ code/ch3_books/config/settings.py
@@ -31,7 +31,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG")
+
+# Calling env("DJANGO_DEBUG") returns 'True' or 'False' as strings not bools,
+# so an equality check is added below.
+DEBUG = env("DJANGO_DEBUG", default=False) == 'True'
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
