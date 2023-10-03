1837dc730fa8191def4e54e1efc98bce07e023ce
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-03 09:25:48 -0300

    Add 'cover' attr to 'Book', apply migration to DB

--- code/ch3_books/books/models.py
+++ code/ch3_books/books/models.py
@@ -14,7 +14,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
+    # Use 'FileField()' for regular files
+    cover = models.ImageField(upload_to='covers/', blank=True)
+
     def __str__(self):
         return self.title
     
