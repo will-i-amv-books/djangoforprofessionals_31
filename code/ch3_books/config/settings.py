36564cd8704e8567a0fa5a7f023a0ed7bb92c5e7
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-03 12:38:56 -0300

    Add configs for cookies suitable for production

--- code/ch3_books/config/settings.py
+++ code/ch3_books/config/settings.py
@@ -190,3 +190,7 @@ SECURE_HSTS_PRELOAD = env.bool(
     "DJANGO_SECURE_HSTS_PRELOAD", 
     default=True
 )
+
+# Cookies
+SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
+CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
